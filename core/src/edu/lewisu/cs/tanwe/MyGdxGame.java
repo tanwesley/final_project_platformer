a269b1db0e949eb3dba8d7cc2809c70482ede883
unknown <tanwesley88@gmail.com>
2020-12-05 15:20:31 -0600

    test

---
+++ core/src/edu/lewisu/cs/tanwe/MyGdxGame.java
@@ -0,0 +1,135 @@
+package edu.lewisu.cs.tanwe;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+import edu.lewisu.cs.cpsc41000.common.EdgeHandler;
+import edu.lewisu.cs.cpsc41000.common.ImageBasedScreenObject;
+import edu.lewisu.cs.cpsc41000.common.ImageBasedScreenObjectDrawer;
+import edu.lewisu.cs.cpsc41000.common.PlatformCharacter;
+import edu.lewisu.cs.cpsc41000.common.labels.ActionLabel;
+
+
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	PlatformCharacter pc;
+	ImageBasedScreenObjectDrawer artist;
+	ArrayList<ImageBasedScreenObject> walls;
+	EdgeHandler edgy;
+	OrthographicCamera cam;
+	float WIDTH, HEIGHT;
+	ActionLabel title;
+	int scene;
+
+	@Override
+	public void create () {
+		WIDTH = Gdx.graphics.getWidth();
+		HEIGHT = Gdx.graphics.getHeight();
+		batch = new SpriteBatch();
+
+		Texture img = new Texture("badlogic.jpg");
+		pc = new PlatformCharacter(img,150,0,false);
+		pc.setMaxSpeed(400);
+		pc.setAcceleration(400);
+		pc.setDeceleration(1600);
+
+		walls = new ArrayList<ImageBasedScreenObject>();
+		Texture wallTex = new Texture("wall.png");
+		walls.add(new ImageBasedScreenObject(wallTex,0,0,true));
+		walls.add(new ImageBasedScreenObject(wallTex,500,0,true));
+		pc.setPlatforms(walls);
+		artist = new ImageBasedScreenObjectDrawer(batch);
+		cam = new OrthographicCamera(WIDTH,HEIGHT);
+		cam.translate(WIDTH/2,HEIGHT/2);
+		cam.update();
+		batch.setProjectionMatrix(cam.combined);
+		edgy = new EdgeHandler(pc,cam,batch,-300,1200,-300,1200,0,
+			EdgeHandler.EdgeConstants.PAN, EdgeHandler.EdgeConstants.PAN);
+		title = new ActionLabel("TITLE", 600, 600, "fonts/arial.fnt");
+		scene = 0;
+	}
+
+	public void renderMain() {
+		Gdx.gl.glClearColor(1,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		float dt = Gdx.graphics.getDeltaTime();
+		Gdx.gl.glClearColor(1,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		if (Gdx.input.isKeyJustPressed(Keys.SPACE) && pc.onSolid()) {
+			pc.jump();
+		}
+		if (Gdx.input.isKeyJustPressed(Keys.ESCAPE)) {
+			scene = 0;
+			return;
+		}
+		if (Gdx.input.isKeyPressed(Keys.W) && pc.onSolid()) {
+			pc.jump();
+		}
+		if (Gdx.input.isKeyPressed(Keys.A)) {
+			pc.accelerateAtAngle(180);
+		}
+		if (Gdx.input.isKeyPressed(Keys.S)) {
+			//pc.accelerateAtAngle(270);
+		}
+		if (Gdx.input.isKeyPressed(Keys.D)) {
+			pc.accelerateAtAngle(0);
+		}
+
+		pc.applyPhysics(dt);
+		Vector2 bounce;
+		for (ImageBasedScreenObject wall : walls) {
+			if (pc.overlaps(wall)) {
+				System.out.println("collision");
+				bounce = pc.preventOverlap(wall);
+				if (bounce != null) {
+					pc.rebound(bounce.angle(),0.1f);
+				}
+			}
+		}
+		edgy.enforceEdges();
+		batch.begin();
+		artist.draw(pc);
+
+		for (ImageBasedScreenObject wall : walls) {
+			artist.draw(wall);
+		}
+
+		batch.end();
+	}	
+
+	public void renderTitleScreen() {
+		Gdx.gl.glClearColor(0,1,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		if (Gdx.input.isKeyJustPressed(Keys.ESCAPE)) {
+			scene = 1;
+		} else {
+			batch.begin();
+			title.draw(batch,1f);
+			batch.end();
+		}
+	}
+
+	@Override
+	public void render () {
+		if (scene == 0) {
+			renderTitleScreen();
+		} else {
+			renderMain();
+		}
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
