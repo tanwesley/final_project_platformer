8ed8d860b4bc1f1193ceae1faa2cc125d4820e32
unknown <tanwesley88@gmail.com>
2020-12-06 21:39:15 -0600

    added music, tutorial labels

--- core/src/edu/lewisu/cs/tanwe/MyGdxGame.java
+++ core/src/edu/lewisu/cs/tanwe/MyGdxGame.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 import edu.lewisu.cs.cpsc41000.common.Boundary;
 import edu.lewisu.cs.cpsc41000.common.EdgeHandler;
@@ -24,6 +25,7 @@ import edu.lewisu.cs.cpsc41000.common.labels.ActionLabel;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	PlatformCharacter pc;
+	PlatformCharacter alien;
 	ImageBasedScreenObjectDrawer artist;
 	ArrayList<ImageBasedScreenObject> walls;
 	ImageBasedScreenObject goal;
@@ -33,13 +35,19 @@ public class MyGdxGame extends ApplicationAdapter {
 	EdgeHandler edgy;
 	OrthographicCamera cam;
 	OrthographicCamera titleCam;
+	OrthographicCamera winCam;
 	float WIDTH, HEIGHT;
 	ActionLabel title;
+	ActionLabel winMessage;
 	ActionLabel startGameLabel;
+	ArrayList<ActionLabel> tutorials;
 	int scene;
 	Texture background;
 
 	int WORLDWIDTH, WORLDHEIGHT;
+	ArrayList<Music> songs;
+
+	Music ashesToAshes;
 	Music lifeOnMars;
 
 	@Override
@@ -59,6 +67,10 @@ public class MyGdxGame extends ApplicationAdapter {
 		pc.setAcceleration(800);
 		pc.setDeceleration(1600);
 
+		// movement tutorial
+		tutorials = new ArrayList<ActionLabel>();
+		tutorials.add(new ActionLabel("W/SPACE - Jump\nA - Move left \nD - Move right", 100, -100, "fonts/arial_small_font.fnt"));
+
 		// Building a test level with platforms
 		walls = new ArrayList<ImageBasedScreenObject>();
 		Texture wallTex = new Texture("wall.png");
@@ -73,17 +85,29 @@ public class MyGdxGame extends ApplicationAdapter {
 		walls.add(new ImageBasedScreenObject(wallTex,1100,0,true));
 		walls.add(new ImageBasedScreenObject(wallTex,1100,wallTex.getHeight(),true));
 		walls.add(new ImageBasedScreenObject(wallTex,1100,wallTex.getHeight()*2,true));
+		walls.add(new ImageBasedScreenObject(wallTex,1000,500,true));
+		walls.add(new ImageBasedScreenObject(wallTex,1000+wallTex.getWidth(),500,true));
+		walls.add(new ImageBasedScreenObject(wallTex,1000+wallTex.getWidth()*2,500,true));
+		walls.add(new ImageBasedScreenObject(wallTex,1000+wallTex.getWidth()*3,500,true));
+		
 
-		//player continues to bounce on these surfaces
+		//player continues to bounce on these surfaces and pressing jump on landing will launch player
+		tutorials.add(new ActionLabel("Press jump while landing on \nblue platforms to bounce high", 600, -40, "fonts/arial_small_font.fnt"));
 		Texture bouncyTex = new Texture("bouncy.png");
 		bouncyPlatforms = new ArrayList<ImageBasedScreenObject>();
 		bouncyPlatforms.add(new ImageBasedScreenObject(bouncyTex,800,100,true));
 
-		// slow the player down
+		// slow the player down and prevents jumping
+		tutorials.add(new ActionLabel("Green platforms slow you down and\nprevent you from jumping", 1500, -40, "fonts/arial_small_font.fnt"));
 		Texture gooTex = new Texture("goo.png");
 		gooPlatforms = new ArrayList<ImageBasedScreenObject>();
 		gooPlatforms.add(new ImageBasedScreenObject(gooTex,1800,0,true));
 		gooPlatforms.add(new ImageBasedScreenObject(gooTex,1800+gooTex.getWidth(),0,true));
+		
+		Texture goalTex = new Texture("goal.png");
+		goal = new ImageBasedScreenObject(goalTex,2500,0,true);
+		winMessage = new ActionLabel("YOU WIN",50,400,"fonts/arial_large_font.fnt");
+
 
 		pc.setPlatforms(walls);
 		artist = new ImageBasedScreenObjectDrawer(batch);
@@ -93,6 +117,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		titleCam = new OrthographicCamera(WIDTH,HEIGHT);
 		titleCam.translate(WIDTH/2,HEIGHT/2);
 		titleCam.update();
+		winCam = new OrthographicCamera(WIDTH,HEIGHT);
+		winCam.translate(WIDTH/2,HEIGHT/2);
+		winCam.update();
 		batch.setProjectionMatrix(cam.combined);
 		edgy = new EdgeHandler(pc,cam,batch,0,WORLDWIDTH,0,WORLDHEIGHT,100,
 			EdgeHandler.EdgeConstants.PAN, EdgeHandler.EdgeConstants.PAN);
@@ -100,10 +127,25 @@ public class MyGdxGame extends ApplicationAdapter {
 		scene = 0;
 		title = new ActionLabel("LIFE ON MARS?",50,400,"fonts/arial_large_font.fnt");
 		startGameLabel = new ActionLabel("Press ESC to begin",150,100,"fonts/arial.fnt");
-		lifeOnMars = Gdx.audio.newMusic(Gdx.files.internal("music/lifeonmars.mp3"));
-		lifeOnMars.setLooping(true);
-		lifeOnMars.setVolume(0.1f);
-		lifeOnMars.play();
+
+
+		//Music
+		songs = new ArrayList<Music>();
+	
+		// win screen music
+		songs.add(Gdx.audio.newMusic(Gdx.files.internal("music/ashestoashes.mp3"))); // 0
+		songs.get(0).setLooping(true);
+		songs.get(0).setVolume(0.1f);
+		
+		// level 1 music
+		songs.add(Gdx.audio.newMusic(Gdx.files.internal("music/lifeonmars.mp3"))); // 1
+		songs.get(1).setLooping(true);
+		songs.get(1).setVolume(0.1f);
+
+		// title screen music
+		songs.add(Gdx.audio.newMusic(Gdx.files.internal("music/starman.mp3"))); // 2
+		songs.get(2).setLooping(true);
+		songs.get(2).setVolume(0.1f);
 	}
 
 	public void renderMain() {
@@ -136,6 +178,10 @@ public class MyGdxGame extends ApplicationAdapter {
 		pc.applyPhysics(dt);
 		Vector2 bounce;
 
+		if (pc.onSolid()) {
+			pc.setMaxSpeed(400); // resets player speed 
+		}
+
 		for (ImageBasedScreenObject wall : walls) {
 			if (pc.overlaps(wall)) {
 				System.out.println("collision");
@@ -152,6 +198,9 @@ public class MyGdxGame extends ApplicationAdapter {
 				bounce = pc.preventOverlap(bouncy);
 				if (bounce != null) {
 					pc.rebound(bounce.angle(),1f);
+					if (Gdx.input.isKeyPressed(Keys.W)) {
+						pc.setMaxSpeed(800); // bounce higher
+					}
 				}
 			}
 		}
@@ -159,13 +208,23 @@ public class MyGdxGame extends ApplicationAdapter {
 		for (ImageBasedScreenObject goo : gooPlatforms) {
 			if (pc.overlaps(goo)) {
 				bounce = pc.preventOverlap(goo);
+				if (bounce != null) {
+					pc.setMaxSpeed(100); // slows player down
+				}
 			}
+		}
+
+		if (pc.overlaps(goal)) {
+			scene = 2; // win screen
 		}
 
 
 		edgy.enforceEdges();
 		batch.begin();
 		batch.draw(background, 0,0);
+		for (ActionLabel tutorial : tutorials) {
+			tutorial.draw(batch,1f);
+		}
 		artist.draw(pc);
 
 		for (ImageBasedScreenObject wall : walls) {
@@ -179,6 +238,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		for (ImageBasedScreenObject goo : gooPlatforms) {
 			artist.draw(goo);
 		}
+
+		artist.draw(goal);
 
 		batch.end();
 	}	
@@ -198,11 +259,33 @@ public class MyGdxGame extends ApplicationAdapter {
 		}
 	}
 
+	public void renderWinScreen() {
+		Gdx.gl.glClearColor(1,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(winCam.combined);
+		batch.begin();
+		batch.draw(background,0,0);
+		winMessage.draw(batch,1f);
+		batch.end();
+	}
+
 	@Override
 	public void render () {
 		if (scene == 1) {
+			songs.get(0).stop();
+			songs.get(2).stop();
+			songs.get(1).play();
 			renderMain();
+		} else if (scene == 2) {
+			songs.get(1).stop();
+			songs.get(2).stop();
+			songs.get(0).play();
+			renderWinScreen();
+
 		} else {
+			songs.get(0).stop();
+			songs.get(1).stop();
+			songs.get(2).play();
 			renderTitleScreen();
 		}
 	}
